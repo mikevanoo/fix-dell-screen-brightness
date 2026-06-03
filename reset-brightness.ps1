8ff93da4b18cd2a37474239abaf39a4ced71e255
Mike van Oorschot <michael.oorschot@codurance.com>
2026-06-03 08:34:25 +0100

    Change to a different brightness first, before setting to required brightness. To see if that forces the change correctly.

--- reset-brightness.ps1
+++ reset-brightness.ps1
@@ -1,1 +1,3 @@
+Get-CimInstance -Namespace root/WMI -ClassName WmiMonitorBrightnessMethods | Invoke-CimMethod -MethodName WmiSetBrightness -Arguments @{Timeout=1; Brightness=40} | Out-Null
+
 Get-CimInstance -Namespace root/WMI -ClassName WmiMonitorBrightnessMethods | Invoke-CimMethod -MethodName WmiSetBrightness -Arguments @{Timeout=1; Brightness=35} | Out-Null
